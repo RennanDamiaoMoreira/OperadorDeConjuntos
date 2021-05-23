8917a7afaed0026a1e19c08997f3dff54597477c
Rennan <rennandamiao@gmail.com>
2021-05-22 21:16:04 -0300

    Update Archive.java

--- src/model/Archive.java
+++ src/model/Archive.java
@@ -1,12 +1,18 @@
 package model;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Archive {
 	static FileWriter fw;
 	static BufferedWriter bw;
 	
 	
 	public static String[] filesDirectory() {
-		File diretorio = new File(System.getProperty("user.dir") + "/export/");
+		File diretorio = new File(System.getProperty("user.dir") + "/modelo/");
 		return diretorio.list();
 	}
 	
@@ -14,7 +20,7 @@ public class Archive {
 	public static String readFile(String statement) {
 		String text = " ";
 		try {
-			File myObj = new File(System.getProperty("user.dir") + "/export/" + statement + ".txt");
+			File myObj = new File(System.getProperty("user.dir") + "/modelo/" + statement + ".txt");
 			Scanner myReader = new Scanner(myObj);
 			while (myReader.hasNextLine()) {
 				String data = myReader.nextLine();
