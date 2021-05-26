9a0815953d364ef86123485c88126e5dca525a39
Rennan <rennandamiao@gmail.com>
2021-05-26 12:37:27 -0300

    Create TestePartes.java

---
+++ src/test/TestePartes.java
@@ -0,0 +1,43 @@
+package test;
+
+import fact.UniverseGenerator;
+import model.*;
+
+import java.util.ArrayList;
+
+public class TestePartes {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < Archive.filesDirectory().length; i++) {
+            System.out.println(Archive.filesDirectory()[i]);
+        }
+
+        System.out.println(Archive.readFile("teste"));
+        UniverseGenerator.generate("teste.txt");
+
+        Universo teste = UniverseGenerator.generate("teste.txt");
+        teste.gerarParte(teste.buscaConjunto("B"));
+
+        Partes p = teste.getPartes().get(0);
+        ArrayList<Conjunto> cPartes = p.getConjuntos();
+        System.out.println(cPartes.size());
+        for (Conjunto c : cPartes) {
+            System.out.println("--------------");
+            if (c != null) {
+                for (Elemento e : c.getElementos()) {
+
+                        System.out.println(e.getValor());
+
+                }
+            }else {
+				System.out.println("Conjunto VAZIO");
+			}
+            System.out.println("--------------");
+        }
+
+
+    }
+
+
+}
