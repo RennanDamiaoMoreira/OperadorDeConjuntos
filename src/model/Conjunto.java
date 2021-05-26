8b9dc3c1a7a5206fd13c253fdcd57543cbdd7e1f
Rennan <rennandamiao@gmail.com>
2021-05-26 13:21:30 -0300

    Update Conjunto.java

--- src/model/Conjunto.java
+++ src/model/Conjunto.java
@@ -3,32 +3,42 @@ package model;
 import java.util.ArrayList;
 
 public class Conjunto {
-	private String nome;
-	private ArrayList<Elemento> elementos = new ArrayList<Elemento>();
-	
-	public Conjunto(String nome, ArrayList<Elemento> elementos) {
-		super();
-		this.nome = nome;
-		this.elementos = elementos;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public ArrayList<Elemento> getElementos() {
-		return elementos;
-	}
-	public void setElementos(ArrayList<Elemento> elementos) {
-		this.elementos = elementos;
-	}
-	
-	public void addElemento(Elemento e ){
-		if (!elementos.contains(e)){
-			elementos.add(e);
-		}
-	}
-	
+    private String nome;
+    private ArrayList<Elemento> elementos = new ArrayList<Elemento>();
+
+    public Conjunto(String nome, ArrayList<Elemento> elementos) {
+        super();
+        this.nome = nome;
+        this.elementos = elementos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Elemento> getElementos() {
+        return elementos;
+    }
+
+    private void setElementos(ArrayList<Elemento> elementos) {
+        this.elementos = elementos;
+    }
+
+    public void addElemento(Elemento e) {
+        boolean permitir = true;
+        for (Elemento ref : elementos) {
+            if (ref == e) {
+                permitir = false;
+                break;
+            }
+        }
+        if (permitir) {
+            elementos.add(e);
+        }
+    }
+
 }
